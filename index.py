1c2d43f59d0870d9bb1515eaa908d6eac4aa6e0f
Laura Colon <lauravanesa2025@gmail.com>
2026-04-08 19:16:13 -0500

    Agrego funciones: agregar producto, ver productos y menú

--- index.py
+++ index.py
@@ -1,17 +1,25 @@
-# main.py
+
 productos = []
 
+def agregar_producto():
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: $"))
+    productos.append({"nombre": nombre, "precio": precio})
+    print(f"'{nombre}' agregado correctamente.")
+
+def ver_productos():
+    if len(productos) == 0:
+        print("No hay productos registrados.")
+    else:
+        print("\n Lista de productos:")
+        for i, p in enumerate(productos, 1):
+            print(f"{i}. {p['nombre']} - ${p['precio']:.2f}")
+
 def menu():
-    print("\n===== 🛒 Sistema de Gestión de Compras =====")
+    print("\n===== Sistema de Gestión de Compras =====")
     print("1. Agregar producto")
     print("2. Ver todos los productos")
     print("3. Calcular total de la compra")
     print("4. Filtrar productos por precio")
     print("5. Salir")
     return input("Selecciona una opción: ")
-
-while True:
-    opcion = menu()
-    if opcion == "5":
-        print("¡Hasta luego!")
-        break
